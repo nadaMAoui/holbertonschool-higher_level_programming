a59a331b57314e2cef4375a9f007bb10351c72e7
nadaMAoui <maaouinada747@gmail.com>
2023-01-17 19:37:55 +0100

    play with strings

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ word_first_3 = word [0:3]
 word_last_2 = word  [-2:]
 middle_word = word [1:-1]
 print("First 3 letters: {}". format(word_first_3))
-print("Word last_2: {}". format(word_last_2))
+print("Last 2 letters: {}". format(word_last_2))
 print("Middle word: {}". format(middle_word))
