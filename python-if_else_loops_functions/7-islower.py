6e2d3a5c8439020fe0004cc4b033d460fb7528c2
nadaMAoui <maaouinada747@gmail.com>
2023-01-19 09:12:36 +0100

    conditions

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range (ord('a'), ord('z')):
+    for i in range(ord('a'), ord('z')):
         if ord(c) != i:
             return False
         else:
