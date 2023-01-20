6d88587bdced7ec3326423ca28dc7bdd7c8394db
nadaMAoui <maaouinada747@gmail.com>
2023-01-20 22:46:46 +0100

    hidden discoveries    ...

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,10 @@ import hidden_4
 
 
 def principal():
-    for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
+    for j in dir(hidden_4):
+        if not (j[0] == '_' and j[1] == '_'):
             print(i)
+
 
 if __name__ == "__main__":
     principal()
