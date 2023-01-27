6c4a6048d88c2d395002a488158f8b602da5edb9
nadaMAoui <maaouinada747@gmail.com>
2023-01-27 16:43:32 +0100

    return key with biggest value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return(None)
-    keys=list(a_dictionary.keys())
-    values=list(a_dictionary.values())
+    keys = list(a_dictionary.keys())
+    values = list(a_dictionary.values())
     return(keys[values.index(max(values))])
