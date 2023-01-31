c30ce1bc3dd0b2662b580cf6a2b3e7331e30cbba
nadaMAoui <maaouinada747@gmail.com>
2023-01-31 09:45:16 +0100

    first task errr ---

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" define a class Square"""
+"""Square Class
+This class don't do anything for the moment
+"""
+
+
 class Square:
-    """Represent a Square"""
-
-
-    def __init__(self):
-        pass
+    pass
