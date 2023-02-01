175726dba430063dd1a125b5e414dfa671064ad0
nadaMAoui <maaouinada747@gmail.com>
2023-02-01 11:39:00 +0100

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,11 +6,11 @@ class Square:
     """Represent a Square"""
     def __init__(self, size=0):
         self.__size = size
-
+    @property
     def size(self):
         """ size private attribute"""
         return (self.__size)
-
+    @size.setter
     def size(self, value):
         """type public attribute"""
         if type(value) is not int:
