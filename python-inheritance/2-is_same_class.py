9f191ad0121ec6922d21514b0105056d5b0f563c
nadaMAoui <maaouinada747@gmail.com>
2023-02-08 15:16:09 +0100

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,8 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    """ 
-    return boolean data_type if the obj is 
+    """
+    return boolean data_type if the obj is
     an instance of the specified class
     """
     return (type(obj) is (a_class))
