30cef11a312fcbdec6bf0b48037133680c883f1e
nadaMAoui <maaouinada747@gmail.com>
2023-02-08 16:02:25 +0100

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""main function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    return a boolean data_type
+    if the object is an instance of a class that
+    inherited from the specified class
+    """
+    if(isinstance(obj, a_class)) and \
+        issubclass(a_class, obj.__class__) is False:
+        return True
+    return False
