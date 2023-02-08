cb97ffb689bcd9525f297a187ca15902df3157f0
nadaMAoui <maaouinada747@gmail.com>
2023-02-08 14:49:26 +0100

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" main class"""
+
+class MyList(list):
+    """My_List class"""
+    def print_sorted(self):
+        """ method to print sorted list"""
+        print(sorted(self))
