00308bea3d2463a0c979daa66b9f258c3b04c2e9
nadaMAoui <maaouinada747@gmail.com>
2023-02-08 16:04:18 +0100

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     inherited from the specified class
     """
     if(isinstance(obj, a_class)) and \
-        issubclass(a_class, obj.__class__) is False:
+       issubclass(a_class, obj.__class__) is False:
         return True
     return False
