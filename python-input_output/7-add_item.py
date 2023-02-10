9c47c782a8555513547dd93844c1a142e9764cd1
nadaMAoui <maaouinada747@gmail.com>
2023-02-10 13:16:59 +0100

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,10 @@ from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
+"""
+script that adds all arguments to a Python list,
+and then save them to a file
+"""
 if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
 else:
