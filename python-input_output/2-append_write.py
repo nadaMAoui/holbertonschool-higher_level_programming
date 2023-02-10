a6228518a3462306bba48ec256e6a39e1612c54b
nadaMAoui <maaouinada747@gmail.com>
2023-02-10 02:23:39 +0100

    file append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,4 +8,4 @@ def append_write(filename="", text=""):
     text file (UTF8) and returns the number of characters added
     """
     with open(filename, encoding="UTF8", mode="w") as my_file:
-        return my_file.write(str(text))
+        return my_file.write(text)
