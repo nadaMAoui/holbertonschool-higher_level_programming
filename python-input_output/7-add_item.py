20370a43dd80c1dda55b7c2dbd1584dd7a79c612
nadaMAoui <maaouinada747@gmail.com>
2023-02-10 13:15:23 +0100

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""main"""
+"""main functions"""
 
 
 from os import path
 from sys import argv
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
