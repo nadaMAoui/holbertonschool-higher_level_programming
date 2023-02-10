af0a45c97a039da7433368ef408b63da144bd09b
nadaMAoui <maaouinada747@gmail.com>
2023-02-10 13:25:12 +0100

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 
 from os import path
 from sys import argv
-
+"""import"""
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """
@@ -18,5 +18,5 @@ else:
 
 for i in range(1, len(argv)):
     obj_json_file.append(argv[i])
-
+""" funct"""
 save_to_json_file(obj_json_file, 'add_item.json')
