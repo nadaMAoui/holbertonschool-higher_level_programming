c9c0807eca88e51a254c7a689a6dba56faf6c8d4
nadaMAoui <maaouinada747@gmail.com>
2023-02-10 15:58:19 +0100

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,7 +5,7 @@
 def class_to_json(obj):
     """
     a function that returns the dictionary description
-    with simple data structure for JSON serialization 
+    with simple data structure for JSON serialization
     of an object
     """
     return (obj.__dict__)
