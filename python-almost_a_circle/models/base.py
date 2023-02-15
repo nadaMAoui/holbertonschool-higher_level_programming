b0f1843fb5279e73063249fdd95c56e9612aabed
nadaMAoui <maaouinada747@gmail.com>
2023-02-15 08:30:22 +0100

    base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,15 +4,16 @@ model class that holds the base class to manager all
 the id attributes of all classesthat extend from the
 base class to avoid duplication of same code
 """
-import os
 
 
 class Base(self):
     """base classe"""
 
     __nb_objects = 0
-    id : int
+
     def __init__(self, id=None):
+        """init"""
+
         if(id is None)
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
