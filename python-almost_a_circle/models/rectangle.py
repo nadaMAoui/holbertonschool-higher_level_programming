bf07077bb470a54f6944602b679ed1414c924773
nadaMAoui <maaouinada747@gmail.com>
2023-02-15 11:30:22 +0100

    full class rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-main class
+Rectangle
 """
 from models.base import Base
 
 
 class Rectangle(Base):
     """
-    Class rectangle
-    inherited from Base
+    main class Rectangle
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -17,7 +16,6 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-
 
     @property
     def width(self):
@@ -38,3 +36,96 @@ class Rectangle(Base):
     def y(self):
         """Get y"""
         return self.__y
+
+    @width.setter
+    def width(self, value):
+        """Set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Ser x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Set y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+
+    def area(self):
+        """
+        calculate the area of rectangle
+        """
+        return self.width * self.height
+
+
+    def display(self):
+        """
+        print matrix
+        """
+        for i in range(self.y):
+            print()
+        for j in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+
+    def __str__(self):
+        """
+        overriding __str__ mthod
+        """
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+
+    def update(self, *args, **kwargs):
+        """
+        adding the public method def update(self, *args):
+        that assigns an argument to each attribute
+        """
+        if args:
+            for i, value in enumerate(args):
+                if i == 0:
+                    self.id = value
+                elif i == 1:
+                    self.width = value
+                elif i == 2:
+                    self.height = value
+                elif i == 3:
+                    self.x = value
+                elif i == 4:
+                    self.y = value
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Update the class Rectangle by adding the
+        public method def to_dictionary(self)
+        """
+        listsq = {'id': self.id, 'width': self.width, 'height': self.height,
+                  'x': self.x, 'y': self.y}
+        return listsq
