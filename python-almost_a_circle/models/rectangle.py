b09cca3346a3ceaf5a7c0d9ca46516c82f77fda7
nadaMAoui <maaouinada747@gmail.com>
2023-02-15 09:08:08 +0100

    rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+main class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Class rectangle
+    inherited from Base
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Get x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Get y"""
+        return self.__y
