caaaf534703112bff4af896af89aea0363d038fc
nadaMAoui <maaouinada747@gmail.com>
2023-02-27 11:38:31 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,42 +4,20 @@ This module performs the addition operation between two numbers,
 these numbers can be integers or floats.
 """
 
-import math
-
-x = float('NaN')
-if math.isnan(x):
-    print('x is NaN')
-else:
-    y = int(x)
-    print('y =', y)
 
 def add_integer(a, b=98):
-    """Adds two numbers
-    Performs the addition between two numbers.
-    Args:
-        a (:obj:`int, float`): The first number.
-        b (:obj:`int, float`, optional): The second number.
-    Returns:
-        int: The result of the addition.
-    """
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
-
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-
-    a = convert_to_int(a)
-    b = convert_to_int(b)
+    """add two numbers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+    a = int(a)
+    b = int(b)
     return a + b
 
 
 def convert_to_int(num):
-    """Cast the data type of num parameter
-    Convert a float number to a integer number
-    Args:
-        num (:obj:`int, float`): The number to cast.
-    Returns:
-        int: The number casted to integer.
+    """Cast the data type of num
     """
     if type(num) is float:
         num = int(num)
