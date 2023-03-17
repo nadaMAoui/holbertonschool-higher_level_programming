fccbf89d2a8dfd9915065f4fd4bb3bdc9252a666
nadaMAoui <maaouinada747@gmail.com>
2023-03-17 23:28:11 +0100

    correction

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows FROM tv_show_genres
-INNER JOIN  tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name ORDER BY number_shows DESC;
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
