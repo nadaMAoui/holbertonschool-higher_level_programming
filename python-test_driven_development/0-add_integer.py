09a99efc0cc80796d19c6a5a97a747964ed01ea0
nadaMAoui <maaouinada747@gmail.com>
2023-03-27 21:17:44 +0200

    test_driven developement

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""A module to add two numbers
-This module performs the addition operation between two numbers,
-these numbers can be integers or floats.
-"""
 
 
 def add_integer(a, b=98):
-    """add two numbers"""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
-    a = cover_to_int(a)
-    b = covr_to_int(b)
-    return a + b
-
-
-def convert_to_int(num):
-    """Cast the data type of num
     """
-    if type(num) is float:
-        num = int(num)
-        return num
-
-    return num
+    add_integer - Adss Two Integers
+    @a: where Type a must be an integer
+    @b: where Type b must be an integer
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return int(a) + int(b)
+    else:
+        raise TypeError("{:} must be an integer"
+                        .format("b" if isinstance(a, (int, float)) else "a"))
