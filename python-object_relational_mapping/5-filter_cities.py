ae37ec9d0fd12d17a2120a16acbe1e333af1577c
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 16:10:29 +0200

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-import sys
+"""Select from database"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL: {}".format(e))
-        sys.exit(1)
-
-    cursor = conn.cursor()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     state_name = sys.argv[4]
-    cursor.execute("""
-        SELECT cities.id, cities.name, states.name FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """, (state_name,))
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    query = "SELECT cities.name FROM cities\
+            LEFT JOIN states ON states.id = cities.state_id\
+            WHERE states.name LIKE BINARY %s\
+            ORDER BY cities.id ASC"
+
+    cursor.execute(query, (state_name,))
+
     rows = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    print(", ".join([city[0] for city in rows]))
 
     cursor.close()
-    conn.close()
+    db.close()
