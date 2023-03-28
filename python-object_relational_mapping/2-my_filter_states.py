3af214021c9bbbb05d94a68ed3aae0b92c11f3c5
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 13:58:20 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password, db=database)
     cursor = conn.cursor()
 
     query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
@@ -26,3 +27,4 @@ if __name__ == "__main__":
 
     cursor.close()
     conn.close()
+
