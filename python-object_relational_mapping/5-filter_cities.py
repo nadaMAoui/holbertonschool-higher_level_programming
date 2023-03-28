6f94a2a8c30078341dab371484d96f2be9c6e218
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 16:08:07 +0200

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,36 @@
-#!/usr/bin/python3
-""" Python x MySQL : Listing data from a database using case sensitive """
+#!/usr/bin/env python3
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s ORDER BY cities.id", (argv[4], ))
-    ls = []
-    for cities in c.fetchall():
-        if cities not in ls:
-            ls.append(cities)
-    print(ls[0][0], end="")
-    ls.remove(ls[0])
-    for x in ls:
-        print(", ", x[0], end="")
-    print("")
-    c.close()
-    db.close()
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL: {}".format(e))
+        sys.exit(1)
+
+    cursor = conn.cursor()
+    state_name = sys.argv[4]
+    cursor.execute("""
+        SELECT cities.id, cities.name, states.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
