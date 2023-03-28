3026c4bd022c6d03b070106b1abee2e4fcf579e6
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 13:08:05 +0200

    python SQL

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python x MySQL : Listing specific data from a database using args """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY\
+              'N%' ORDER BY states.id")
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
