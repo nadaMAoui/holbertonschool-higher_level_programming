5b72c97bad738f6fc35dabc3ca8794db3b54dc93
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 15:57:13 +0200

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-""" Python x MySQL : Listing data from join tables """
+"""
+A script that lists all cities from the database hbtn_0e_4_usa.
+"""
+
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id")
-    for state in c.fetchall():
-        print(state)
-    c.close()
-    db.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+
+    cursor = conn.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
+se()
