c4e77ccc62b248a7002daa619120f3f06833809e
nadaMAoui <maaouinada747@gmail.com>
2023-03-28 15:39:10 +0200

    task3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-""" Python x MySQL : Listing data from a database using case sensitive """
+"""
+A script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * from states WHERE name LIKE %s ORDER BY states.id",
-              (argv[4],))
-    for state in c.fetchall():
-        print(state)
-    c.close()
-    db.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+
+    cursor.execute(query, ('%' + state_name + '%',))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
