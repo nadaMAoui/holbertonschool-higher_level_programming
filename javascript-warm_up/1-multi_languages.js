19f42134430b33a3c716fe77c23029e91a0bdd04
nadaMAoui <maaouinada747@gmail.com>
2023-04-19 17:21:42 +0200

    print 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// print 3 strings
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+console.log(line1 + '\n' + line2 + '\n' + line3);
