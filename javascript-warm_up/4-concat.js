a7e461fa2e4361b49ae9cbd955958218a113aa6d
nadaMAoui <maaouinada747@gmail.com>
2023-04-20 11:53:38 +0200

    task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-//a script that prints two arguments passed to it, in the following format: “ is ”
+// a script that prints two arguments passed to it, in the following format: “ is ”
 
 if (process.argv[2] && process.argv[3]) {
   console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else { console.log('undefined is undefined'); }
+} else { console.log('HBTN is undefined'); }
