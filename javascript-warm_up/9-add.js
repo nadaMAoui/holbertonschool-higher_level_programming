b050c45240d20c396433278458b739b310e93dcf
nadaMAoui <maaouinada747@gmail.com>
2023-04-20 21:38:37 +0200

    task 9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// a script that prints the addition of 2 integers
+
+function add (a, b) {
+  const sum = a + b;
+  console.log('The sum of', a, 'and', b, 'is:', sum);
+}
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+add(num1, num2);
