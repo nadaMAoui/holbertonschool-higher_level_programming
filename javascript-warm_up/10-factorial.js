b5df63aae212ec60190ea61e2d1f810582a3ed24
nadaMAoui <maaouinada747@gmail.com>
2023-04-20 21:52:31 +0200

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// a script that computes and prints a factorial
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+const input = isNaN(num) ? 1 : num;
+
+const result = factorial(input);
+
+console.log('The factorial of', input, 'is:', result);
