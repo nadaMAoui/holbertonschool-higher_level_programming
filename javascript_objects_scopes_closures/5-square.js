3583c638645cefdeb4944418a298505706789594
nadaMAoui <maaouinada747@gmail.com>
2023-04-21 20:22:45 +0200

    inheritemce

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -3,8 +3,8 @@
 // a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 
 class Square extends require('./4-rectangle') {
-    constructor (length) {
-      super(length, length);
-    }
+  constructor (length) {
+    super(length, length);
   }
-  module.exports = Square;
+}
+module.exports = Square;
