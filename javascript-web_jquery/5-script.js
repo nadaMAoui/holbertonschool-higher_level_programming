70119fa0c8f637f773a56cab57ab463749db7ddc
nadaMAoui <maaouinada747@gmail.com>
2023-04-28 22:02:59 +0200

    jquery

---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,5 @@
+// updating
+const $ = window.$;
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+//  toggles the class of the HTML tag HEADER when the user clicks on the tag DIV#toggle_header
+const $ = window.$;
+$('DIV#toggle_header').click(function () {
+  $('HEADER').toggleClass('red green');
+});
